e90cc02e3054ab55b8b18cda14d5dd9f5566f9ad
lakshmijeybe97@gmail.com <lakshmijeybe97@gmail.com>
2024-02-13 14:46:50 -0500

    ToDos Component and filter fix

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,38 +1,102 @@
-import React, {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { useState, useRef } from "react";
+import ToDos from "./ToDos";
+import "bootstrap/dist/css/bootstrap.css";
 
 const AddTask = () => {
-    const [addtask,setAddTask] = useState("");
-    const [tasksadded,setTasksAdded] = useState([]);
-    const handleInputChange = (e) =>{
-          setAddTask(e.target.value);
+  const [addtask, setAddTask] = useState("");
+  const [tasksadded, setTasksAdded] = useState([]);
+  const [togglebutton, setToggleButton] = useState("Add Task");
+  const [editIndex, setEditIndex] = useState(null);
+  const [checkedItems, setCheckedItems] = useState({});
+  const [taskdisplay, setTaskDisplay] = useState(false);
+  const inputRef = useRef();
+  const handleInputChange = (e) => {
+    setAddTask(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    if (addtask.trim() !== "") {
+      if (togglebutton === "Add Task") {
+        setTasksAdded((prevTasksAdded) => [...prevTasksAdded, addtask]);
+      } else {
+        const toUpdateTasks = [...tasksadded];
+        toUpdateTasks[editIndex] = addtask;
+        setTasksAdded(toUpdateTasks);
+      }
     }
-    const handleSubmit = (e) =>{
-        if(addtask.trim()!== ""){
-            setTasksAdded((prevTasksAdded)=>([...prevTasksAdded,addtask]) );
-        }
-        e.preventDefault();
-        setAddTask("");
-    }
-    
-    return(
-        <div class="container text-center">
-            <h2 class="header">ToDo App</h2>
-
-            <form class="row justify-content-center align-items-center" onSubmit={handleSubmit}>
-                <input type="text" class="inputwidth col-6 m-1 g-5 form-control" value={addtask} onChange={handleInputChange}></input>
-                <button type="submit" class="buttonclass btn btn-primary">Add Task</button>
-            </form>
-            
-        
-            <ul class="list-group">
-            {
-               tasksadded.map(((taskadded,index) =>( <li class="list-group-item d-flex justify-content-between align-items-center" key={index}><input type="checkbox"></input>{taskadded}<span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1em" height="1em"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></span>
-               </li>)))
-            }
-            </ul>
-        </div>
+    e.preventDefault();
+    setAddTask("");
+    setToggleButton("Add Task");
+  };
+  const handleEdit = (editIndex) => {
+    setToggleButton("Edit");
+    setAddTask(tasksadded[editIndex]);
+    console.log(addtask);
+    setEditIndex(editIndex);
+    console.log(editIndex);
+  };
+  const handleDelete = (deleteTask) => {
+    setTasksAdded((prevTasksAdded) =>
+      prevTasksAdded.filter((task) => task !== deleteTask)
     );
-}
+  };
+  const handleCheck = (checkedIndex) => {
+    setCheckedItems((prevCheckedState) => ({
+      ...prevCheckedState,
+      [checkedIndex]: !prevCheckedState[checkedIndex],
+    }));
+  };
+  const ShowAll = () => {
+    setTaskDisplay(false);
+  };
+  const ShowCompleted = () => {
+    setTaskDisplay(true);
+  };
+  console.log(taskdisplay);
+  const filtered = taskdisplay
+    ? tasksadded.filter((task, taskIndex) => checkedItems[taskIndex])
+    : tasksadded;
+  return (
+    <div class="container d-flex flex-column gap-2 text-center">
+      <h2 class="header">ToDo App</h2>
+
+      <form
+        class="row justify-content-center align-items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          class="inputwidth col-6 m-1 form-control"
+          value={addtask}
+          onChange={handleInputChange}
+        ></input>
+        <button type="submit" class="buttonclass btn btn-primary ">
+          {togglebutton}
+        </button>
+      </form>
+      <div class="d-flex flex-row gap-2 justify-content-center">
+        <button class="filterbutton btn btn-primary" onClick={ShowAll}>
+          All Tasks
+        </button>
+        <button class="filterbutton btn btn-primary" onClick={ShowCompleted}>
+          Completed
+        </button>
+      </div>
+      <ul class="list-group d-flex justify-content-center liststyle">
+        {filtered.map((taskadded, index) => (
+          <ToDos
+            key={index}
+            index={index}
+            handleEdit={handleEdit}
+            taskadded={taskadded}
+            handleDelete={handleDelete}
+            handleCheck={handleCheck}
+            checkedItems={checkedItems}
+            taskdisplay={taskdisplay}
+          ></ToDos>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default AddTask;
---
+++ src/Components/ToDos.js
@@ -0,0 +1,72 @@
+import React from "react";
+import DeleteIcon from "./assets/DeleteIcon";
+import EditIcon from "./assets/EditIcon";
+const ToDos = ({
+  index,
+  handleEdit,
+  taskadded,
+  handleDelete,
+  handleCheck,
+  checkedItems,
+  taskdisplay,
+}) => {
+  return (
+    <div>
+      {!taskdisplay ? (
+        <li
+          class={`mainliststyle list-group-item d-flex justify-content-between align-items-center ${
+            checkedItems[index] ? "bg-success" : ""
+          }`}
+          key={index}
+        >
+          <div class="taskliststyle">
+            <input
+              type="checkbox"
+              checked={checkedItems[index]}
+              onChange={() => handleCheck(index)}
+            ></input>
+            <div>{taskadded}</div>
+          </div>
+          <div class="taskliststyle">
+            <button
+              class="editclass btn btn-primary"
+              onClick={() => handleDelete(taskadded)}
+            >
+              <DeleteIcon></DeleteIcon>
+            </button>
+            <button
+              class="editclass btn btn-primary"
+              onClick={() => handleEdit(index)}
+            >
+              <EditIcon></EditIcon>
+            </button>
+          </div>
+        </li>
+      ) : (
+        <li
+          class={`mainliststyle list-group-item d-flex justify-content-between align-items-center`}
+          key={index}
+        >
+          <div class="taskliststyle">
+            <div>{taskadded}</div>
+          </div>
+          <div class="taskliststyle">
+            <button
+              class="editclass btn btn-primary"
+              onClick={() => handleDelete(taskadded)}
+            >
+              <DeleteIcon></DeleteIcon>
+            </button>
+            <button
+              class="editclass btn btn-primary"
+              onClick={() => handleEdit(index)}
+            >
+              <EditIcon></EditIcon>
+            </button>
+          </div>
+        </li>
+      )}
+    </div>
+  );
+};
+export default ToDos;
